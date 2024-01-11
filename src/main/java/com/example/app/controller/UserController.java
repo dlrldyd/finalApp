abe3f6954bbb457277c42084fc19809e96c469dd
dlrldyd <osamuelo@naver.com>
2024-01-11 15:52:45 +0900

    settings

---
+++ src/main/java/com/example/app/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.app.controller;
+
+import com.example.app.domain.dto.UserDto;
+import com.example.app.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+}
